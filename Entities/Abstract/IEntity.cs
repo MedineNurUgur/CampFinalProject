2853e5a31cc92a88c8df65f14cf149726bb7554b
medine nur uğur <medinenurugur669@gmail.com>
2021-02-03 11:52:55 +0300

    Firs Comiit

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Bu interface'i implement edec class bitr veri tabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
