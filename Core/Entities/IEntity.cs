9cad8954d4d13df66cb3d82b55fbb13520730d74
medine nur uğur <medinenurugur669@gmail.com>
2021-02-08 14:47:16 +0300

    Adding Core Layer

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //Bu interface'i implement edec class bitr veri tabanı tablosudur.
     public interface IEntity
