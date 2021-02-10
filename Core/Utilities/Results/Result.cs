70976d87a6dc320bad843fe89893f8d2dc3c2a2b
medine nur uğur <medinenurugur669@gmail.com>
2021-02-10 23:35:55 +0300

    Added operation results

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //İKi kere success = success yazdık buda kendini tekrar etme ilkesine ters bunun için ilk constructer'dan diğerini çağırdık.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public bool Success { get; }
+
+        public string Message { get; }
+        
+    }
+}
