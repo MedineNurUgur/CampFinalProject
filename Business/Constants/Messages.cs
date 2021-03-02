a296b6f24cb4fbbe18640940b31cef0476ca264f
medine nur uğur <medinenurugur669@gmail.com>
2021-03-02 16:40:33 +0300

    Added Authentication

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductsListed = "Ürünler listelendi.";
         public static string ProductCountOfCategotoryError = "Bir kategoride 10 dan fazla ürün olamaz.";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered;
+        public static string UserNotFound;
+        public static string PasswordError;
+        public static string SuccessfulLogin;
+        public static string UserAlreadyExists;
+        public static string AccessTokenCreated;
     }
 }
