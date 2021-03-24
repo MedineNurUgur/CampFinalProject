02711665403cf345a71fbcc017676321a37ff86a
medine nur uğur <medinenurugur669@gmail.com>
2021-03-24 18:56:44 +0300

    Editted

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -29,7 +30,7 @@ namespace WebAPI.Controllers
         public IActionResult Get()
         {
             // Dependency chain
-            
+            Thread.Sleep(1000);
             var result = _productService.GetAll();
 
             if (result.Success)
@@ -40,10 +41,21 @@ namespace WebAPI.Controllers
 
         }
 
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
         [HttpGet("getbyid")]
         public IActionResult Get(int id)
         {
-            var result = _productService.GetById(id);
+            var result = _productService.GetByCategoryId(id);
             if (result.Success)
             {
                 return Ok(result);
